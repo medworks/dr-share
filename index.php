943c79ac47c6281874c4fe8987314b5a6ba57edb
mojiAm <amjadi.mojtaba@gmail.com>
2014-10-08 11:40:13 +0330

    add meta tag

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+	<title>کلینیک رهیاب</title>
+	<meta charset="UTF-8" />
+	<meta name="google-site-verification" content="" />
+	<meta name="msvalidate.01" content="" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="Mediateq.ir" />
+	<meta name="googlebot" content="INDEX,FOLLOW" />
+	<meta name="robots" content="INDEX,FOLLOW" />
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+</head>
+<body>
+
+</body>
+</html>
