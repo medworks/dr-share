0cad664a886c0116c884c9e0a4b8f66cdc10ad0d
mojiAm <amjadi.mojtaba@gmail.com>
2014-10-14 10:50:57 +0330

    add js & css files

--- index.php
+++ index.php
@@ -11,11 +11,68 @@
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<meta name="author" content="Mediateq.ir" />
+	<meta name="generator" content="Powered by Mediateq CMS panel">
 	<meta name="googlebot" content="INDEX,FOLLOW" />
 	<meta name="robots" content="INDEX,FOLLOW" />
-	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
-</head>
-<body>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="format-detection" content="telephone=yes">
+	
+	<link rel="shortcut icon" href="./favicon.ico">
+	<link rel="stylesheet" id="css3_grid_table1_style-css" href="./css/css3_grid_style.css" type="text/css" media="all">
+	<link rel="stylesheet" id="css3_grid_table2_style-css" href="./css/css3_grid_style2.css" type="text/css" media="all">
+	<link rel="stylesheet" id="css3_grid_responsive-css" href="./css/responsive.css" type="text/css" media="all">
+	<link rel="stylesheet" id="reset-css" href="./css/reset.css" type="text/css" media="all">
+	<link rel="stylesheet" id="superfish-css" href="./css/superfish.css" type="text/css" media="all">
+	<link rel="stylesheet" id="jquery-fancybox-css" href="./css/jquery.fancybox.css" type="text/css" media="all">
+	<link rel="stylesheet" id="jquery-qtip-css" href="./css/jquery.qtip.css" type="text/css" media="all">
+	<link rel="stylesheet" id="jquery-ui-custom-css" href="./css/jquery-ui-1.9.2.custom.css" type="text/css" media="all">
+	<link rel="stylesheet" id="main-style-css" href="./css/style.css" type="text/css" media="all">
+	<link rel="stylesheet" id="responsive-css" href="./css/responsive2.css" type="text/css" media="all">
+	<link rel="stylesheet" id="rtl-css" href="./css/rtl.css" type="text/css" media="all">
+	<link rel="stylesheet" id="custom-css" href="./css/custom.css" type="text/css" media="all">
+	<link rel="stylesheet" href="./css/rtl.css" type="text/css" media="screen">
+	<link rel="canonical" href="">
+	<link rel="shortlink" href="">
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
 
+	<script type="text/javascript" src="./js/jquery.js"></script>
+	<script type="text/javascript" src="./js/jquery-migrate.min.js"></script>
+	<script type="text/javascript" src="./js/rtl.js"></script>
+
+</head>
+<body class="rtl home page page-id-5 page-template page-template-template-home-php wpb-js-composer js-comp-ver-4.1.3 vc_responsive">
+
+<!--custom style-->
+<style type="text/css">
+</style>
+
+<link rel="stylesheet" id="js_composer_front-css" href="./css/js_composer_front.css" type="text/css" media="all">
+
+<script type="text/javascript" src="./js/jquery.ui.core.min.js"></script>
+<script type="text/javascript" src="./js/jquery.ui.widget.min.js"></script>
+<script type="text/javascript" src="./js/jquery.ui.accordion.min.js"></script>
+<script type="text/javascript" src="./js/jquery.ui.tabs.min.js"></script>
+<script type="text/javascript" src="./js/jquery.ui.datepicker.min.js"></script>
+<script type="text/javascript" src="./js/jquery.ba-bbq.min.js"></script>
+<script type="text/javascript" src="./js/jquery.history.js"></script>
+<script type="text/javascript" src="./js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="./js/jquery.carouFredSel-6.2.1-packed.js"></script>
+<script type="text/javascript" src="./js/jquery.sliderControl.js"></script>
+<script type="text/javascript" src="./js/jquery.timeago.js"></script>
+<script type="text/javascript" src="./js/jquery.hint.js"></script>
+<script type="text/javascript" src="./js/jquery.isotope.min.js2"></script>
+<script type="text/javascript" src="./js/jquery.isotope.masonry.js"></script>
+<script type="text/javascript" src="./js/jquery.fancybox-1.3.4.pack.js"></script>
+<script type="text/javascript" src="./js/jquery.qtip.min.js"></script>
+<script type="text/javascript" src="./js/jquery.blockUI.js"></script>
+<script type="text/javascript">
+	/* <![CDATA[ */
+	var config = [];
+	config = {"ajaxurl":"http:\/\/quanticalabs.com\/wp_themes\/medicenter\/wp-admin\/admin-ajax.php","themename":"medicenter","home_url":"http:\/\/quanticalabs.com\/wp_themes\/medicenter","is_rtl":1};;
+	/* ]]> */
+</script>
+<script type="text/javascript" src="./js/main.js"></script>
+<script type="text/javascript" src="./js/js_composer_front.js"></script>
+	
 </body>
 </html>
---
+++ js/rtl.js
@@ -0,0 +1,6 @@
+jQuery((function($){
+	// Overwriting Isotope core method
+	$.Isotope.prototype._positionAbs = function(a, b) {
+		return{right: a, top: b};
+	}
+})(jQuery));
