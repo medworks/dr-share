813b71da57aca51cab488342ab5deb6b2f4bc662
Saeid Hatami <hatami4510@gmail.com>
2014-10-27 20:51:44 +0330

    add code to categories

--- manager/categories.php
+++ manager/categories.php
@@ -1,6 +1,26 @@
 <?php
-    include_once("./inc/header.php")
-?>
+	include_once("../config.php");
+	include_once("../classes/functions.php");
+  	include_once("../classes/messages.php");
+  	include_once("../classes/session.php");	
+  	include_once("../classes/security.php");
+  	include_once("../classes/database.php");	
+	include_once("../classes/login.php");
+    include_once("../lib/persiandate.php"); 
+
+	$login = Login::GetLogin();
+    if (!$login->IsLogged())
+	{
+		header("Location: ../index.php");
+		die(); // solve a security bug
+	}
+	$db = Database::GetDatabase();	
+	
+	if ($_POST["mark"]=="saveact")
+	{
+	}
+
+$html=<<<cd
     <!--Page main section start-->
     <section id="min-wrapper">
         <div id="main-content">
@@ -31,6 +51,7 @@
                                         <input type="text" class="form-control" placeholder="اسم گروه" />
                                     </div>
                                     <button type="submit" class="btn btn-default">ثبت</button>
+									<input type="hidden" name="mark" value="savecat" />
                                 </form>
                             </div>
                         </div>
@@ -91,6 +112,8 @@
         </div>
     </section>
     <!--Page main section end -->
-<?php
-    include_once("./inc/footer.php")
+cd;
+	include_once("./inc/header.php");
+	echo $html;
+    include_once("./inc/footer.php");
 ?>
