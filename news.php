c11b225461cc7062108d75d7bb9fef6f38949a22
Saeid Hatami <hatami4510@gmail.com>
2014-11-18 22:51:38 +0330

    compelete news page

--- news.php
+++ news.php
@@ -1,31 +1,50 @@
 <?php
 	include_once("config.php");
 	include_once("classes/functions.php");
-  	include_once("classes/messages.php");
-  	include_once("classes/session.php");	
   	include_once("classes/security.php");
   	include_once("classes/database.php");	
-	include_once("classes/login.php");
-    include_once("lib/persiandate.php"); 
+    include_once("./lib/persiandate.php");
+	include_once("./lib/Zebra_Pagination.php"); 	
 	
 	//error_reporting(E_ALL);
 	//ini_set('display_errors', 1);
 	
 	$db = Database::GetDatabase();
 	
-	$news = $db->SelectAll("news","*",NULL,"id ASC");
+	$records_per_page = 10;
+	$pagination = new Zebra_Pagination();
 
-$html.=<<<cd
+	$pagination->navigation_position("right");
+
+	$reccount = $db->CountAll("news");
+	$pagination->records($reccount); 
+	
+    $pagination->records_per_page($records_per_page);	
+
+$rows = $db->SelectAll(
+				"news",
+				"*",
+				NULL,
+				"id ASC",
+				($pagination->get_page() - 1) * $records_per_page,
+				$records_per_page);
+				
+
+$nhtml.=<<<cd
 <div id="main" class="col9 clearfix">
 	<div id="main_inner">
 		<div class="article_grid four_column_blog">
 			<h4>اخبار و تازه ها</h4>
+cd;
 
-			
+for($i = 0; $i < Count($rows); $i++)
+{
+	$rows[$i]["regdate"] = ToJalali($rows[$i]["regdate"],"Y/m/d H:i");
+$nhtml.=<<<cd
 			<div class="col3">
 				<div>
 					<div class="article_grid_image">
-						<img class="morph" src="./images/slides/1.jpg" alt="" width="280" height="160" />
+						<img class=" morph" src="manager/img.php?did={$rows[$i]["id"]}&tid=2" width="280px" height="160px" />
 						<div class="hover_buttons">
 							<div>
 								<div>
@@ -37,21 +56,15 @@ $html.=<<<cd
 					<div class="article_grid_content">
 						<h3 class="article_heading">
 							<a href="#" title="" rel="bookmark">
-								عنوان خبر
+								{$rows[$i]["subject"]}
 							</a>
 						</h3>
 						<p class="post_meta">
 							<span class="meta_date">
-								<a href="#" title="">29 فروردین 1393</a>
-							</span>
-							<span class="meta_author">
-								<a href="#">Admin</a>
-							</span>
+								<a href="#" title="">{$rows[$i]["regdate"]}</a>
+							</span>							
 						</p>
-						<div class="post_excerpt">
-							<p>
-								خبر پنجم
-							</p>
+						<div class="post_excerpt">							
 							<p>
 								<a class="post_more_link" href="#">ادامه خبر</a>
 							</p>
@@ -59,14 +72,18 @@ $html.=<<<cd
 					</div>
 				</div>
 			</div>
-
-
+cd;
+}
+$pgcodes = $pagination->render(true);
+$nhtml.=<<<cd
 		</div>
 	</div><!-- #main_inner -->
+	{$pgcodes}
 </div>
 cd;
 
 	include_once('./inc/header.php');
+	echo $nhtml;
 	include_once('./inc/sidebar.php');
 	include_once('./inc/footer.php');
 	include_once('./inc/last.php');
