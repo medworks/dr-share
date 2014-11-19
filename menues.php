93bbb892f9d640528e6c99d704896cf6ea070f33
Saeid Hatami <hatami4510@gmail.com>
2014-11-20 02:08:06 +0330

    add menues page

---
+++ menues.php
@@ -0,0 +1,53 @@
+<?php
+	include_once("config.php");
+	include_once("classes/functions.php");
+  	include_once("classes/messages.php");
+  	include_once("classes/session.php");	
+  	include_once("classes/security.php");
+  	include_once("classes/database.php");	
+	include_once("classes/login.php");
+    include_once("lib/persiandate.php"); 
+	
+	//error_reporting(E_ALL);
+	//ini_set('display_errors', 1);
+	
+	$db = Database::GetDatabase();
+	
+	$news = $db->Select("menusubjects","*","smid={$_GET['id']}");
+	$news["regdate"] = ToJalali($news["regdate"],"Y/m/d H:i");
+$ohtml.=<<<cd
+<div id="main" class="col9 clearfix">
+	<div id="main_inner">
+
+		<div class="single_post_module">
+			<div class="post type-post status-publish format-standard has-post-thumbnail hentry category-featured category-lifestyle category-travel tag-adipiscing tag-augue tag-donec tag-etiam tag-euismod">
+				<div class="single_post_image">					
+					<a href="manager/img.php?did={$news['id']}&tid=1" class="hb-image-zoom" rel="swipebox[single_post_image]" title="">
+					  <img class=" morph" src="manager/img.php?did={$news['id']}&tid=1" width="874px" height="492px" />	
+					</a>
+					<div class="meta_category">
+						<a href="javascript:void();" class="cat-featured" title="{$news['subject']}">تاریخ : {$news["regdate"]}</a>						
+					</div>
+				</div>
+				<article class="single_post_content">
+					<h1 class="article_heading entry_title" style="font-size:35px !important">{$news['subject']}</h1>
+					<div class="clearboth"></div>
+					<div class="entry">
+						<p style="font-size:22px;font-weight:normal;">
+							{$news["text"]}
+						</p>
+						<div class="clearboth"></div>									
+					</div>
+				</article>
+			</div>
+		</div>
+	</div><!-- #main_inner -->
+</div>
+cd;
+
+	include_once('./inc/header.php');
+	echo $ohtml;
+	include_once('./inc/sidebar.php');
+	include_once('./inc/footer.php');
+	include_once('./inc/last.php');
+?>
