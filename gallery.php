5ff9b3f290b1396cac003712342800c3fe4eae94
Saeid Hatami <hatami4510@gmail.com>
2014-11-20 13:06:59 +0330

    add code to gallery page

--- gallery.php
+++ gallery.php
@@ -6,36 +6,59 @@
   	include_once("classes/security.php");
   	include_once("classes/database.php");	
 	include_once("classes/login.php");
-    include_once("lib/persiandate.php"); 
+    include_once("lib/persiandate.php");
+    include_once("./lib/Zebra_Pagination.php"); 	
 	
 	//error_reporting(E_ALL);
 	//ini_set('display_errors', 1);
 	
-	$db = Database::GetDatabase();
-	
+	$db = Database::GetDatabase();	
 
-$html.=<<<cd
+$ghtml.=<<<cd
 <div id="main" class="col9 clearfix">
 	<div id="main_inner">
 		<div class="article_grid four_column_blog">
 			<h4>گالری تصاویر</h4>
-			
+cd;
 
+
+	$records_per_page = 10;
+	$pagination = new Zebra_Pagination();
+
+	$pagination->navigation_position("right");
+
+	$reccount = $db->CountAll("gcategories");
+	$pagination->records($reccount); 
+	
+    $pagination->records_per_page($records_per_page);	
+
+$grows = $db->SelectAll(
+				"gcategories",
+				"*",
+				NULL,
+				"id ASC",
+				($pagination->get_page() - 1) * $records_per_page,
+				$records_per_page);
+for($i = 0; $i < Count($grows); $i++)
+{
+$gpics = $db->Select("gallerypics","*","gcid = {$grows[$i]['id']}");
+//echo $db->cmd;
+$ghtml.=<<<cd
 			<div class="col4">
 				<div>
 					<div class="article_grid_image">
-						<img class="morph" src="./images/slides/5.jpg" alt="" width="480" height="270">
+						<img class="morph" src="manager/img.php?did={$gpics['id']}&type=gall" width="480px" height="270px" /> 						
 						<div class="hover_buttons">
 							<div>
 								<div>
-									<a href="./images/slides/5.jpg" class="hb-image-zoom" rel="swipebox[portfolio_img_group_13]" title=""></a>
+									<a href="manager/img.php?did={$gpics['id']}&type=gall" class="hb-image-zoom" rel="swipebox[portfolio_img_group_13]" title=""></a>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="article_grid_content">
 						<h3 class="article_heading">
-							<a href="#">عنوان سرگروه عکس</a>
+							<a href="#">آلبوم : {$grows[$i]['name']}</a>
 						</h3>
 						<div class="post_excerpt">
 							<p>
@@ -45,14 +68,18 @@ $html.=<<<cd
 					</div>
 				</div>
 			</div>
-
-
+cd;
+}
+$pgcodes = $pagination->render(true);
+$ghtml.=<<<cd
 		</div>
+		{$pgcodes}
 	</div><!-- #main_inner -->
 </div>
 cd;
 
 	include_once('./inc/header.php');
+	echo $ghtml;
 	include_once('./inc/sidebar.php');
 	include_once('./inc/footer.php');
 	include_once('./inc/last.php');
