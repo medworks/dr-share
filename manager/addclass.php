2dfa9582007c47ca63ccefa0799447bffb3904ec
Saeid Hatami <hatami4510@gmail.com>
2014-12-15 00:43:21 +0330

    add code to addclass file

--- manager/addclass.php
+++ manager/addclass.php
@@ -6,7 +6,7 @@
   	include_once("../classes/security.php");
   	include_once("../classes/database.php");	
 	include_once("../classes/login.php");
-    include_once("../lib/persiandate.php"); 
+	include_once("../lib/persiandate.php"); 
 
 	$login = Login::GetLogin();
     if (!$login->IsLogged())
@@ -15,66 +15,11 @@
 		die(); // solve a security bug
 	}
 	$db = Database::GetDatabase();
-	function upload($db,$did,$mode)
-	{
-		if(is_uploaded_file($_FILES['userfile']['tmp_name']) && getimagesize($_FILES['userfile']['tmp_name']) != false)
-		{    
-			$size = getimagesize($_FILES['userfile']['tmp_name']);		
-			$type = $size['mime'];
-			$imgfp = mysql_real_escape_string(file_get_contents($_FILES['userfile']['tmp_name']));
-			//echo $imgfp;
-			$size = $size[3];
-			$name = $_FILES['userfile']['name'];
-			$maxsize = 512000;//512 kb
-			//$db = Database::GetDatabase();
-			//echo $db->cmd;
-			if($_FILES['userfile']['size'] < $maxsize )
-			{    
-				//echo "my1";
-				//tid 1 is for menu pics, 2 for news pics, 3 for maghalat pics
-				if ($mode == "insert")
-				{
-					$fields = array("`tid`","`sid`","`itype`","`img`","`iname`","`isize`");		
-					$values = array("'1'","'{$did}'","'{$type}'","'{$imgfp}'","'{$name}'","'{$size}'");	
-					$db->InsertQuery('pics',$fields,$values);
-				}
-				else
-				{
-				  $imgrow =$db->Select("pics","*","sid='{$did}'");
-				  if ($imgfp != $imgrow["img"])
-				  {
-					$values = array("`tid`"=>"'1'","`sid`"=>"'{$did}'",
-						"`itype`"=>"'{$type}'","`img`"=>"'{$imgfp}'",
-						"`iname`"=>"'{$name}'","`isize`"=>"'{$size}'");
-					$db->UpdateQuery("pics",$values,array("sid='{$did}'"));	
-				  }	
-				}	
-				//echo $db->cmd;
-			}
-			else
-			{        
-				throw new Exception("File Size Error");
-			}
-		}
-		else
-		{		
-			throw new Exception("Unsupported Image Format!");
-		}
-	}	
+	
 	
 	if ($_POST["mark"]=="savedata")
 	{
-		if (isset($_POST["cbsm2"]) and $_POST["cbsm2"]!=0)
-		{
-			$sm = $_POST["cbsm2"];
-		}
-		else
-		if (isset($_POST["cbsm1"]) and $_POST["cbsm1"]!=0)
-		{
-			$sm = $_POST["cbsm1"];
-		}
-		$date = date('Y-m-d H:i:s');
-		$fields = array("`mid`","`smid`","`subject`","`text`","`regdate`","`picid`");		
+		$fields = array("`title`","`subjects`","`starttime`","`period`","`endtime`","`details`");		
 		$values = array("'{$_POST[cbmenu]}'","'{$sm}'","'{$_POST[edtsubject]}'","'{$_POST[edttext]}'","'{$date}'","'0'");	
 		if (!$db->InsertQuery('menusubjects',$fields,$values)) 
 		{			
@@ -82,32 +27,18 @@
 		} 	
 		else 
 		{  
-			if ($_FILES['userfile']['tmp_name']!="")
-			{
-				$did = $db->InsertId();
-				upload($db,$did,"insert");
 				header('location:dataentry.php?act=new&msg=1');
-			}	
 		}  		
 	}
 	else
 	if ($_POST["mark"]=="editdata")
 	{		
-		if (isset($_POST["cbsm2"]) and $_POST["cbsm2"]!=0)
-		{
-			$sm = $_POST["cbsm2"];
-		}
-		else
-		if (isset($_POST["cbsm1"]) and $_POST["cbsm1"]!=0)
-		{
-			$sm = $_POST["cbsm1"];
-		}
 		
-		$values = array("`mid`"=>"'{$_POST[cbmenu]}'","`smid`"=>"'{$sm}'",
-						"`subject`"=>"'{$_POST[edtsubject]}'","`text`"=>"'{$_POST[edttext]}'",
-						"`picid`"=>"'0'");
-        $db->UpdateQuery("menusubjects",$values,array("id='{$_GET[did]}'"));
-		upload($db,$_GET["did"],"edit");	
+		$values = array("`title`"=>"'{$_POST[cbmenu]}'","`subjects`"=>"'{$sm}'",
+				"`starttime`"=>"'{$_POST[edtsubject]}'","`period`"=>"'{$_POST[edttext]}'",
+				"`endtime`"=>"'0'","`details`"=>"'{$_POST[edttext]}'");
+		$db->UpdateQuery("menusubjects",$values,array("id='{$_GET[did]}'"));
+		
 		header('location:dataentry.php?act=new&msg=1');
 	}
 	
@@ -116,96 +47,17 @@
 		$insertoredit = "
 			<button id='submit' type='submit' class='btn btn-default'>ثبت</button>
 			<input type='hidden' name='mark' value='savedata' /> ";
-		$menues = $db->SelectAll("menues","*");	
-		$cbmenu = DbSelectOptionTag("cbmenu",$menues,"name",NULL,NULL,"form-control",NULL,"  منو  ");	
 	}
 
-	if ($_GET['act']=="view")
-	{
-	    $row=$db->Select("menusubjects","*","id='{$_GET["did"]}'",NULL);
 		
-		$menues = $db->SelectAll("menues","*");	
-		$cbmenu = DbSelectOptionTag("cbmenu",$menues,"name","{$row[mid]}",NULL,"form-control",NULL,"  منو  ");
-		
-		$srow=$db->Select("submenues","*","id='{$row["smid"]}'",NULL);
-		if ($srow["pid"] == 0)	
-		{
-			$m = $srow["mid"];
-			$m1 = $srow["id"];
-			$m2 = 0;
-		}
-		else
-		{			
-			$srow2 = $db->Select("submenues","*","id='{$srow["pid"]}'",NULL);
-			if ($srow2["pid"] == 0)
-			{
-				$m1 = $srow["pid"];
-				$m2 = $srow["id"];
-			}
-			else
-			{
-				$m1 = 0;
-				$m2 = 0;
-			}	
-		}
-		
-		$sm1 = $db->SelectAll("submenues","*","pid = 0");	
-		$cbsm1 = DbSelectOptionTag("cbsm1",$sm1,"name","{$m1}",NULL,"form-control",NULL,"زیر منو");	
-
-		$sm2 = $db->SelectAll("submenues","*","pid <> 0");	
-		$cbsm2 = DbSelectOptionTag("cbsm2",$sm2,"name","{$m2}",NULL,"form-control",NULL,"زیر منو");	
-		
-		$pic = $db->Select("pics","*","sid='{$_GET["did"]}' AND tid = 1",NULL);
-		if (isset($pic))
-		{
-			$imgload = "<img  src='img.php?did={$_GET[did]}&tid=1'  width='200px' height='180px' />";
-		}	
-	}
-	
 	if ($_GET['act']=="edit")
 	{
 	    $row=$db->Select("menusubjects","*","id='{$_GET["did"]}'",NULL);		
 		$insertoredit = "
 			<button id='submit' type='submit' class='btn btn-default'>ویرایش</button>
 			<input type='hidden' name='mark' value='editdata' /> ";
-
-		$menues = $db->SelectAll("menues","*");	
-		$cbmenu = DbSelectOptionTag("cbmenu",$menues,"name","{$row[mid]}",NULL,"form-control",NULL,"  منو  ");
-		
-		$srow=$db->Select("submenues","*","id='{$row["smid"]}'",NULL);
-		if ($srow["pid"] == 0)	
-		{
-			$m = $srow["mid"];
-			$m1 = $srow["id"];
-			$m2 = 0;
-		}
-		else
-		{			
-			$srow2 = $db->Select("submenues","*","id='{$srow["pid"]}'",NULL);
-			if ($srow2["pid"] == 0)
-			{
-				$m1 = $srow["pid"];
-				$m2 = $srow["id"];
-			}
-			else
-			{
-				$m1 = $srow["id"];
-				$m2 = $srow2["pid"];
-			}	
-		}
-		
-		$sm1 = $db->SelectAll("submenues","*","pid = 0");	
-		$cbsm1 = DbSelectOptionTag("cbsm1",$sm1,"name","{$m1}",NULL,"form-control",NULL,"زیر منو");	
-
-		$sm2 = $db->SelectAll("submenues","*","pid <> 0");	
-		$cbsm2 = DbSelectOptionTag("cbsm2",$sm2,"name","{$m2}",NULL,"form-control",NULL,"زیر منو");	
-		
-		$pic = $db->Select("pics","*","sid='{$_GET["did"]}' AND tid = 1",NULL);
-		if (isset($pic))
-		{
-			$imgload = "<img  src='img.php?did={$_GET[did]}&tid=1'  width='200px' height='180px' />";
-		}	
-	}	
+	}
+	
 $html=<<<cd
     <!--Page main section start-->
     <section id="min-wrapper">
