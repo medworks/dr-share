7d619d52908ac521eeb5a7b3394de8304a9fe744
Saeid Hatami <hatami4510@gmail.com>
2014-12-19 00:56:12 +0330

    add code & fix err

--- manager/regconfdetail.php
+++ manager/regconfdetail.php
@@ -21,6 +21,9 @@
     if (isset($_GET["act"]) and $_GET["act"]=="view")
     {
 	$row = $db->Select("hamayesh","*","id ={$_GET['did']}");
+	$pic = $db->Select("clspics","*","cid='{$_GET['did']}' AND tid='2' ");
+	$img = base64_encode($pic['img']);
+	$src = 'data: '.$pic['itype'].';base64,'.$img;
 	$regdate = ToJalali($row["regdate"]," l d F  Y ساعت H:i");
         if($row["tahol"] ==0)
         {
@@ -195,7 +198,7 @@ $html=<<<cd
                                 </div>
                                 <div class="panel-body">
                                     <div class="form-group">
-                                        <input id="edtfish" name="edtfish" type="text" class="form-control" value="{$row["title"]}"/>
+									<img class="morph" src="{$src}"  style="width:201px !important;height:115px !important" />                                    
                                     </div>
                                 </div>
                             </div>
