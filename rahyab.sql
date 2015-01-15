1883be0b103cbb62dcb86d7ad912ef5d030ae79f
Saeid Hatami <hatami4510@gmail.com>
2015-01-15 21:49:17 +0330

    ADD NEW DATABASE

--- rahyab.sql
+++ rahyab.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2015 at 09:39 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Generation Time: Jan 15, 2015 at 07:17 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `rahyab`
 --
+CREATE DATABASE IF NOT EXISTS `rahyab` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `rahyab`;
 
 -- --------------------------------------------------------
 
@@ -366,7 +368,7 @@ CREATE TABLE IF NOT EXISTS `menusubjects` (
   `mid` int(11) NOT NULL,
   `smid` int(11) NOT NULL,
   `subject` varchar(100) NOT NULL,
-  `text` text NOT NULL,
+  `text` mediumtext NOT NULL,
   `regdate` datetime NOT NULL,
   `picid` int(11) NOT NULL,
   PRIMARY KEY (`id`)
@@ -501,8 +503,8 @@ CREATE TABLE IF NOT EXISTS `newsmember` (
   `email` varchar(30) NOT NULL,
   `tell` varchar(12) NOT NULL,
   `mobile` varchar(12) NOT NULL,
-  `menu` varchar(50) NOT NULL,
-  `group` varchar(50) NOT NULL,
+  `menu` varchar(100) NOT NULL,
+  `group` varchar(100) NOT NULL,
   `regdate` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
