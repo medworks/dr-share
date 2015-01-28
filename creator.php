45afd603950ae3b719440a924e5fff602a21c14b
mojiAm <amjadi.mojtaba@gmail.com>
2015-01-28 12:06:23 +0330

    add creator

---
+++ creator.php
@@ -0,0 +1,13 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+	   
+
+$html=<<<cd
+<p style="font-size:22px;text-align:right;direction:rtl">موسس (دکتر حسین شاره)</p>
+<p style="font-size:18px;text-align:right;direction:rtl">توضیحات</p>
+
+
+cd;
+echo $html;	
+	
+?>
